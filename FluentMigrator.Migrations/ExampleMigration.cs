137e61501214a04e8ccf795aeaf09c1f33cfbafa
Emil Slavov <easlavov@gmail.com>
2018-11-17 20:48:36 +0200

    Added stored procedure with comments

--- FluentMigrator.Migrations/ExampleMigration.cs
+++ FluentMigrator.Migrations/ExampleMigration.cs
@@ -6,10 +6,12 @@
         public override void Up()
         {
             Execute.EmbeddedScript("FluentMigrator.Migrations.UserDefinedTables.sql");
+            Execute.EmbeddedScript("FluentMigrator.Migrations.Max.sql");
         }
 
         public override void Down()
         {
+            Execute.Sql("DROP PROCEDURE [dbo].[Max]");
             Execute.Sql("DROP VIEW [dbo].[UserDefinedTables]");
         }
     }
