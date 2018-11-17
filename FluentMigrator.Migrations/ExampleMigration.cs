79172affc928fe726770e2036e6df902df9df34c
Emil Slavov <easlavov@gmail.com>
2018-11-17 20:16:20 +0200

    Added example migration.

---
+++ FluentMigrator.Migrations/ExampleMigration.cs
@@ -0,0 +1,16 @@
+﻿namespace FluentMigrator.Migrations
+{
+    [Migration(1)]
+    public class ExampleMigration : Migration
+    {
+        public override void Up()
+        {
+            Execute.EmbeddedScript("FluentMigrator.Migrations.UserDefinedTables.sql");
+        }
+
+        public override void Down()
+        {
+            Execute.Sql("DROP VIEW [dbo].[UserDefinedTables]");
+        }
+    }
+}
---
+++ FluentMigrator.Migrations/UserDefinedTables.sql
@@ -0,0 +1,4 @@
+﻿/*This view is written by Jon Doe on 17-Nov-2018*/
+CREATE VIEW [dbo].[UserDefinedTables] AS
+	SELECT * FROM sys.types
+		WHERE is_user_defined = 1 -- sample inline comment
