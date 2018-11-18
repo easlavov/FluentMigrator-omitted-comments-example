feb5b8040ac8eaf3c55a0f2bfcb5e8cff8abab19
Emil Slavov <easlavov@gmail.com>
2018-11-18 19:45:27 +0200

    Added a console app that executed a script which creates a stored procedure on a target server using ADO.NET.

---
+++ src/AdoNet.ExecuteScript/Max.sql
@@ -0,0 +1,8 @@
+﻿/*This procedure is written by Jon Doe on 17-Nov-2018*/
+CREATE PROCEDURE [dbo].[Max]
+    @First int,   
+    @Second int   
+AS   
+    if @First > @Second -- sample inline comment
+		return @First
+    return isnull(@Second, @First)
---
+++ src/AdoNet.ExecuteScript/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.IO;
+
+namespace AdoNet.ExecuteScript
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connString = BuildConnectionString(args);
+            var connection = new SqlConnection(connString);
+
+            using (connection)
+            {
+                ExecuteScript(connection, @".\Max.sql");
+            }
+        }
+
+        private static void ExecuteScript(SqlConnection connection, string path)
+        {
+            string query = File.ReadAllText(path);
+
+            var command = connection.CreateCommand();
+            command.CommandText = query;
+
+            try
+            {
+                connection.Open();
+                command.ExecuteNonQuery();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Script execution failed!");
+            }
+
+            Console.WriteLine("Script execution complete!");
+        }
+
+        private static string BuildConnectionString(string[] args)
+        {
+            var connStringBuilder = new SqlConnectionStringBuilder();
+            connStringBuilder.DataSource = args[0];
+            connStringBuilder.InitialCatalog = args[1];
+            connStringBuilder.IntegratedSecurity = true;
+
+            return connStringBuilder.ToString();
+        }
+    }
+}
